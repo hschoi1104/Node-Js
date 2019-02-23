5caa9896e4c3746bc570ee843dc2f5f46426f675
hschoi1104 <hschoi1104@gmail.com>
2019-02-23 19:56:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,46 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-/*
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    <meta http-equiv="refresh" content="15">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-    <p><img src="/imgs" width="500" height="500"></p>
-  </body>
-  </html>
-  `;
-}
-*/
-/*
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - nodejs 가 싫어요</title>
-    <meta charset="utf-8">
-    <meta http-equiv="refresh" content="10">
-  </head>
-  <body>
-    <h1><a href="/">nodejs 가 싫어요</a></h1>
-    <p><img onclick="window.location.reload()" src="/imgs" width="100%" height="100%"></p>
-  </body>
-  </html>
-  `;
-}
-*/
-function templateHTML(title, list, body, control) {
+function templateHTML() {
   return `
   <!DOCTYPE html>
   <html>
@@ -106,159 +67,48 @@ function templateHTML(title, list, body, control) {
   </html>`;
 }
 
-function templateList(filelist) {
-  var list = '<ul>';
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  list = list + '</ul>';
-  return list;
-}
-
+//서버를 생성하는 객체를 만듬
 var app = http.createServer(function(request, response) {
+  //_url 변수에 현재 url를 저장함
   var _url = request.url;
+  //_url 에서 쿼리 데이터만을 저장함
   var queryData = url.parse(_url, true).query;
+  //_url 에서 path만을 저장함
   var pathname = url.parse(_url, true).pathname;
+  //path가 루트 디렉터리 인경우
   if (pathname === '/') {
+    //쿼리데이터가 정의된게 아니라면(없다면)
     if (queryData.id === undefined) {
-      fs.readdir('../#13', function(error, filelist) {
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
+        var template = templateHTML();
+        //200은 상태코드
+        //명시적 헤더 데이터전송
+        //헤더 작성 메서드
         response.writeHead(200);
+        //헤더의 마지막 비트를 지정
+        //헤더의 마지막 데이터로 template(html)을 전송함
         response.end(template);
-      });
     }
     else {
       fs.readdir('../project', function(error, filelist) {
-        fs.readFile(`../project/${queryData.id}`, 'utf8', function(err, description) {
-          var title = queryData.id;
-          var list = templateList(filelist);
-          var template = templateHTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>
-            <form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <input type="submit" value="delete">
-            </form>
-            `
-          );
+          var template = templateHTML();
+          //200은 상태코드
           response.writeHead(200);
           response.end(template);
-        });
       });
     }
-    /*
-  } else if (pathname === '/create') {
-    fs.readdir('../#13', function(error, filelist) {
-      var title = 'WEB - create';
-      var list = templateList(filelist);
-      var template = templateHTML(title, list, `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `, '');
-      response.writeHead(200);
-      response.end(template);
-    });
-  } else if (pathname === '/create_process') {
-    var body = '';
-    request.on('data', function(data) {
-      body = body + data;
-    });
-    request.on('end', function() {
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`../#13/${title}`, description, 'utf8', function(err) {
-        response.writeHead(302, {
-          Location: `/?id=${title}`
-        });
-        response.end();
-      })
-    });
-    //update form
-  } else if (pathname === '/update') {
-    fs.readdir('../#13', function(error, filelist) {
-      fs.readFile(`../#13/${queryData.id}`, 'utf8', function(err, description) {
-        var title = queryData.id;
-        var list = templateList(filelist);
-        var template = templateHTML(title, list,
-          `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(template);
-      });
-    });
-    //update process
-  } else if (pathname === '/update_process') {
-    var body = '';
-    request.on('data', function(data) {
-      body = body + data;
-    });
-    request.on('end', function() {
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`../#13/${id}`, `../#13/${title}`, function(error) {
-        fs.writeFile(`../#13/${title}`, description, 'utf8', function(err) {
-          response.writeHead(302, {
-            Location: `/?id=${title}`
-          });
-          response.end();
-        })
-      })
-    });
-  } else if (pathname === '/delete_process') {
-    var body = '';
-    request.on('data', function(data) {
-      body = body + data;
-    });
-    request.on('end', function() {
-      var post = qs.parse(body);
-      var id = post.id;
-      fs.unlink(`../#13/${id}`, function(error) {
-        response.writeHead(302, {
-          Location: `/`
-        });
-        response.end();
-      });
-    });
-  } else
-  */
+    
 }else if(pathname === '/imgs'){
     fs.readFile('coding2.jpg',function(error,data){
-      response.writeHead(200,{'Content-Type': 'text/html'});
+      //200은 상태코드
+      response.writeHead(200);
       response.end(data);
     })
+    //에러페이지 조건
   }else {
+    //404은 상태코드
     response.writeHead(404);
     response.end('Not found');
   }
 });
+//3000번 포트에 서버를 실행시킨다.
 app.listen(3000);
