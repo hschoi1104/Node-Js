2fb1897263d2905d64932022cb9fdf657aa448c9
hschoi1104 <hschoi1104@gmail.com>
2020-08-10 16:29:18 +0900

    add eslint

--- Node-JWT-Login/app.js
+++ Node-JWT-Login/app.js
@@ -1,35 +1,35 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var connect = require('connect')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var connect = require("connect");
 
 var app = express();
-var config = require('./config')
+var config = require("./config");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')))
-app.use('/api', require('./routes/api'));
+app.use("/api", require("./routes/api"));
 
 /**
  * Set JWT KEY
  */
-app.set('jwt-secret',config.secret)
+app.set("jwt-secret", config.secret);
 
 /**
  * Connect Mongodb
  */
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-    // CONNECTED TO MONGODB SERVER
-    console.log("Connected to mongod server");
+db.on("error", console.error);
+db.once("open", function () {
+  // CONNECTED TO MONGODB SERVER
+  console.log("Connected to mongod server");
 });
 
 mongoose.connect(config.mongodbUri);
--- Node-JWT-Login/models/User.js
+++ Node-JWT-Login/models/User.js
@@ -1,39 +1,44 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const config = require('../config')
-const crypto = require('crypto')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const config = require("../config");
+const crypto = require("crypto");
 
 const User = new Schema({
-    username: String,
-    password: String,
-    admin: {type: Boolean, default: false}
-})
+  username: String,
+  password: String,
+  admin: { type: Boolean, default: false },
+});
 
-User.statics.create = function(username, password){
-    const encrypted = crypto.createHmac('sha1',config.secret).update(password).digest('base64')
+User.statics.create = function (username, password) {
+  const encrypted = crypto
+    .createHmac("sha1", config.secret)
+    .update(password)
+    .digest("base64");
 
-    const user = new this({
-        username,
-        password:encrypted
-    })
-    return user.save()
-}
+  const user = new this({
+    username,
+    password: encrypted,
+  });
+  return user.save();
+};
 
-User.statics.findOneByUsername = function(username){
-    return this.findOne({
-        username
-    }).exec()
-}
+User.statics.findOneByUsername = function (username) {
+  return this.findOne({
+    username,
+  }).exec();
+};
 
+User.methods.verify = function (password) {
+  const encrypted = crypto
+    .createHmac("sha1", config.secret)
+    .update(password)
+    .digest("base64");
+  return this.password === encrypted;
+};
 
-User.methods.verify = function(password){
-    const encrypted = crypto.createHmac('sha1',config.secret).update(password).digest('base64')
-    return this.password === encrypted
-}
+User.methods.assignAdmin = function () {
+  this.admin = true;
+  return this.save();
+};
 
-User.methods.assignAdmin = function(){
-    this.admin = true
-    return this.save()
-}
-
-module.exports = mongoose.model('User',User)
+module.exports = mongoose.model("User", User);
